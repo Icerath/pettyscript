cfbc45ed79b053cd17833cca627f70005ab65ee7
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-01-28 01:13:22 +0200

    cleaned up macro a bit

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,41 +1,41 @@
 use proc_macro::TokenStream;
-use proc_macro2::TokenStream as TokenStream2;
-use quote::quote;
 
 #[proc_macro_attribute]
 pub fn pettyfunc(_attr: TokenStream, input: TokenStream) -> TokenStream {
-    let input_func = syn::parse_macro_input!(input as syn::ItemFn);
-    let (variables, args) = downcast_args(&input_func.sig);
-    let name = &input_func.sig.ident;
-    let vis = &input_func.vis;
-    quote!(
+    let input = syn::parse_macro_input!(input as syn::ItemFn);
+    let syn::ReturnType::Type(_, return_type) = &input.sig.output else {
+        panic!();
+    };
+
+    let variables = downcast_args(&input.sig);
+    let (body, name, vis) = (input.block, input.sig.ident, input.vis);
+
+    quote::quote!(
         #vis fn #name <'__a> (
             args: crate::prelude::FnArgs<'__a>
         ) -> crate::prelude::PettyObject {
-            #[inline]
-            #input_func
             #variables
-            #name ( #args ).into()
+            #return_type::into(#body)
         }
     )
     .into()
 }
 
-fn downcast_args(sig: &syn::Signature) -> (TokenStream2, TokenStream2) {
-    let mut out_args = TokenStream2::new();
-    let mut variables = TokenStream2::new();
-    for (arg_index, arg) in sig.inputs.iter().enumerate() {
-        let syn::FnArg::Typed(arg) = arg else {
-            panic!()
-        };
-        let (name, typ) = (&arg.pat, &arg.ty);
-
-        variables.extend(quote!(
-            let PettyObject::#typ(#name) = args.slice[#arg_index].clone() else {
-                unimplemented!();
+fn downcast_args(sig: &syn::Signature) -> proc_macro2::TokenStream {
+    sig.inputs
+        .iter()
+        .enumerate()
+        .map(|(index, arg)| {
+            let syn::FnArg::Typed(arg) = arg else {
+                panic!()
             };
-        ));
-        out_args.extend(quote!(#name,));
-    }
-    (variables, out_args)
+            let (name, typ) = (&arg.pat, &arg.ty);
+
+            quote::quote!(
+                let PettyObject::#typ(#name) = &args.slice[#index] else {
+                    unimplemented!();
+                };
+            )
+        })
+        .collect()
 }
