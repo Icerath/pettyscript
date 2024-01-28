e4d484d9761e07c06a58bd9fc517fca4642acf68
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-01-28 03:53:09 +0200

    refactored prelude

--- vm/src/prelude.rs
+++ vm/src/prelude.rs
@@ -1,16 +1,13 @@
 pub use crate::fn_args::FnArgs;
-pub use crate::object::ObjectExt;
-pub use crate::object::{CustomObject, Object, PettyObject};
+pub use crate::object::{CustomObject, Object, ObjectExt, PettyObject};
 pub use crate::object::{Float, Int, Null, PtyStr, RawFunc};
 pub use crate::stdlib::StdObject;
-
-pub use std::any::TypeId;
-pub use std::sync::{Arc, Mutex};
 
 pub use arcstr::literal as ptystr;
 
 pub const NULL: &PettyObject = &PettyObject::Null(Null);
 
-// Convenience
 pub use macros::pettyfunc;
+pub use std::any::TypeId;
 pub use std::fmt::Debug;
+pub use std::sync::{Arc, Mutex};
