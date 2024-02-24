e1dda58365f6414dd5564b72ef142cbfcda31bba
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-02-24 02:39:16 +0200

    added clap parsing to cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use std::{fs, path::PathBuf, process::ExitCode};
+
+use clap::Parser;
+use parser::parse_many;
+
+#[derive(Parser)]
+struct Args {
+    path: PathBuf,
+}
+
+fn main() -> ExitCode {
+    let args = Args::parse();
+    let Ok(input) = fs::read_to_string(&args.path) else {
+        eprintln!("ERROR: Failed to read {:?}", args.path);
+        return ExitCode::FAILURE;
+    };
+    let ast = parse_many(&input);
+    eprintln!("{ast:?}");
+    ExitCode::SUCCESS
+}
