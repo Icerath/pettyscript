6ee4ac7c6c55ac5f95157bfea56c1c8e5248aa67
Dorje Gilfillan <dorjegilfillan@gmail.com>
2025-01-29 09:17:08 +0200

    fix FIXME

--- src/intern.rs
+++ src/intern.rs
@@ -1,11 +1,8 @@
 use std::sync::RwLock;
 
-use hashbrown::HashSet;
-use rustc_hash::FxBuildHasher;
+use rustc_hash::{FxBuildHasher, FxHashSet};
 
-// FIXME: use std::collection::HashSet when const HashSet::with_hasher is stabilized.
-static MAP: RwLock<HashSet<&'static str, FxBuildHasher>> =
-    RwLock::new(HashSet::with_hasher(FxBuildHasher));
+static MAP: RwLock<FxHashSet<&'static str>> = RwLock::new(FxHashSet::with_hasher(FxBuildHasher));
 
 pub fn intern(str: &str) -> &'static str {
     if let Some(istr) = MAP.read().unwrap().get(str) {
