92c6105ebd7253f31ff7941fa4ebeeb6d9832c2c
Dorje Gilfillan <dorjegilfillan@gmail.com>
2025-02-13 08:21:19 +0200

    nit

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,5 +1,6 @@
 #![expect(unsafe_op_in_unsafe_fn, reason = "I'm lazy")]
 
+mod value;
 mod variable_stack;
 
 use core::fmt;
@@ -10,85 +11,16 @@ use std::{
     fmt::Write as _,
     hint::{assert_unchecked, unreachable_unchecked},
     io::{self, Write},
-    ops::Deref,
     rc::Rc,
 };
 
+use value::{Callable, PettyMap, PettyStr, Value};
 use variable_stack::VariableStack;
 
 use crate::{
     builtints::{Builtin, MethodBuiltin},
     bytecode::{Instr, StrIdent, VERSION},
 };
-
-pub type PettyMap<'src> = BTreeMap<Value<'src>, Value<'src>>;
-
-// TODO: Remove Rc<Refcell> with indexes
-// TODO: Replace BTreeMap with HashMap
-// TODO: Avoid extra indirection/allocation for Struct.
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub enum Value<'src> {
-    Bool(bool),
-    EnumVariant { tag: u16 },
-    Char(char),
-    Int(i64),
-    Callable(Callable),
-    String(PettyStr<'src>),
-    Array(Rc<RefCell<Vec<Value<'src>>>>),
-    Map(Rc<RefCell<PettyMap<'src>>>),
-    Range([i64; 2]),
-    RangeInclusive([i64; 2]),
-    Struct { fields: Rc<RefCell<Box<[Value<'src>]>>> },
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub enum Callable {
-    Builtin(Builtin),
-    Function { label: u32, stack_size: u16 },
-}
-
-#[derive(Debug, Clone)]
-pub enum PettyStr<'src> {
-    Literal(&'src str),
-    String(Rc<Box<str>>),
-}
-
-impl Deref for PettyStr<'_> {
-    type Target = str;
-
-    fn deref(&self) -> &Self::Target {
-        self.as_str()
-    }
-}
-
-impl PartialEq for PettyStr<'_> {
-    fn eq(&self, other: &Self) -> bool {
-        self.as_str() == other.as_str()
-    }
-}
-
-impl Eq for PettyStr<'_> {}
-
-impl PartialOrd for PettyStr<'_> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.as_str().cmp(other.as_str()))
-    }
-}
-
-impl Ord for PettyStr<'_> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.as_str().cmp(other.as_str())
-    }
-}
-
-impl PettyStr<'_> {
-    fn as_str(&self) -> &str {
-        match *self {
-            Self::Literal(str) => str,
-            Self::String(ref str) => str,
-        }
-    }
-}
 
 /// # Safety
 /// bytecode must have been generated using codegen
---
+++ src/vm/value.rs
@@ -0,0 +1,72 @@
+use std::{cell::RefCell, cmp::Ordering, collections::BTreeMap, ops::Deref, rc::Rc};
+
+use crate::builtints::Builtin;
+
+pub type PettyMap<'src> = BTreeMap<Value<'src>, Value<'src>>;
+
+// TODO: Remove Rc<Refcell> with indexes
+// TODO: Replace BTreeMap with HashMap
+// TODO: Avoid extra indirection/allocation for Struct.
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Value<'src> {
+    Bool(bool),
+    EnumVariant { tag: u16 },
+    Char(char),
+    Int(i64),
+    Callable(Callable),
+    String(PettyStr<'src>),
+    Array(Rc<RefCell<Vec<Value<'src>>>>),
+    Map(Rc<RefCell<PettyMap<'src>>>),
+    Range([i64; 2]),
+    RangeInclusive([i64; 2]),
+    Struct { fields: Rc<RefCell<Box<[Value<'src>]>>> },
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Callable {
+    Builtin(Builtin),
+    Function { label: u32, stack_size: u16 },
+}
+
+#[derive(Debug, Clone)]
+pub enum PettyStr<'src> {
+    Literal(&'src str),
+    String(Rc<Box<str>>),
+}
+
+impl Deref for PettyStr<'_> {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        self.as_str()
+    }
+}
+
+impl PartialEq for PettyStr<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.as_str() == other.as_str()
+    }
+}
+
+impl Eq for PettyStr<'_> {}
+
+impl PartialOrd for PettyStr<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.as_str().cmp(other.as_str()))
+    }
+}
+
+impl Ord for PettyStr<'_> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.as_str().cmp(other.as_str())
+    }
+}
+
+impl PettyStr<'_> {
+    pub fn as_str(&self) -> &str {
+        match *self {
+            Self::Literal(str) => str,
+            Self::String(ref str) => str,
+        }
+    }
+}
